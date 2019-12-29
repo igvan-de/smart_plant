8ee6c02ef63bf107e4be530ae423d6282117c7a7
igvan-de <igvan-de@DESKTOP-CB1EK1H.localdomain>
2019-12-29 12:12:26 +0100

    added code for waterpump

--- moister_sensor.ino
+++ moister_sensor.ino
@@ -1,22 +1,26 @@
-int sensorPin = A2;
-int sesorValue;
-int limit = 300;
+#define sensorPin A2
+#define waterPin A1
+#define limit 300
+#define waterdelay 750
 
-void setup() {
-Serial.begin(9600);
-pinMode(13, OUTPUT);
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
 }
 
-void loop() {
-int sensorValue = analogRead (sensorPin);
-Serial.println("Analog Value : ");
-Serial.println(sensorValue);
-if (sensorValue<limit){
-  Serial.println("LOW");
-  digitalWrite(13, HIGH);
+void loop()
+{
+  int sensorValue = analogRead (sensorPin);
+  Serial.println("Analog Value : ");
+  Serial.println(sensorValue);
+  if (sensorValue<limit){
+    digitalWrite(13, HIGH);
+    digitalWrite(waterPin, HIGH);
+    Serial.println("Waterpump running");
+    delay(waterdelay);
+    digitalWrite(waterPin, LOW);
+    Serial.println("Waterpump not running");
 }
-else {
-  digitalWrite(13,LOW);
-}
-delay(1000);
+delay(5000);
 }
